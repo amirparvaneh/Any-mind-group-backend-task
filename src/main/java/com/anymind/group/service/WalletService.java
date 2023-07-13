b3df1f65cd5822614048a39105bd93f647ea55fd
amirparvaneh <amirparvaneh138@gmail.com>
2023-07-13 23:27:23 +0330

    dev-v1

--- src/main/java/com/anymind/group/service/WalletService.java
+++ src/main/java/com/anymind/group/service/WalletService.java
@@ -3,14 +3,10 @@ package com.anymind.group.service;
 import com.anymind.group.dto.BtcResponseDto;
 import com.anymind.group.model.BTC;
 import com.anymind.group.repository.BtcRepo;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class WalletService {
@@ -34,8 +30,8 @@ public class WalletService {
         return prepareBtcResponse(allByDateTimeBetween);
     }
 
-    private List<BtcResponseDto> prepareBtcResponse(List<BTC> btcs){
-       return btcs.stream().map(btc -> BtcResponseDto.builder()
+    private List<BtcResponseDto> prepareBtcResponse(List<BTC> btcList){
+       return btcList.stream().map(btc -> BtcResponseDto.builder()
                     .amount(btc.getAmount())
                     .date(btc.getDateTime())
                     .build()).toList();
